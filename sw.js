c22d2e17de8a1c91deb2821028925b72b9211f10
yerrojuabhinaysai24-maker <yerrojuabhinaysai24@gmail.com>
2026-08-23 16:28:49 +0530

    Add files via upload
    V10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="orbit-v9-shell";
+const CACHE="orbit-v10-shell";
 const APP_FILES=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
